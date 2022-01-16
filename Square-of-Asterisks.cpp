681021bac54783ef7f29d169f5e37092926aff99
samhelal <93845103+samhelal@users.noreply.github.com>
2022-01-16 21:36:22 +0200

    Update Square-of-Asterisks.cpp

--- Square-of-Asterisks.cpp
+++ Square-of-Asterisks.cpp
@@ -12,7 +12,9 @@ your program reads a size of 5, it should print
 using namespace std ;
 
 int main(){
+    
     int counter(1) , side (0) ,row(1) ;      //initialize the counter to (1) and the square side value to (0) and the row wight counter(1). 
+    
     cout << "Enter side : (from 1 to 20)" ;  // prompet message to ask the user how tall and weight of square u want .
     cin >> side ; //input the side value .
     
@@ -26,24 +28,21 @@ int main(){
             cout<<endl<<endl ;
         }
 
+        
         if(counter != 1){             // if the counter not the first 
             if(counter != side){      // if the counter not the final
-            
-            cout << "*";              // print just one  (*) in the left rectangle side  
-            while(row<=side-2){       //while row less than or equal the side value -2 (dont print the first and the final )
-
-            
+            cout << "*";              // print just one  (*) in the left rectangle side                
+            while(row<=side-2){       //while row less than or equal the side value -2 (dont print the first and the final ) (*    *)
                 cout<<" ";            //prin blanks between the two asterisks . 
                 row++;                // add one to the row counter 
-            
             }
             row = 1 ;                // revalue the row to 1 to use it again in the next .
             cout << "*";             // print the right side 
             cout << endl << endl ;
-
             }
         }
 
+        
         if(counter == side){            //if the counter equal final , to print just the bottom side full asterisk ***** .
             while(row<=side){           //while row less than or equal side .
                 cout << "*" ;           
